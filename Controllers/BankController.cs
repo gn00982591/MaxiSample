cd821e03ec1cf400039ae95625548d42b107db11
gn00982591 <gn00982591@msn.com>
2017-10-06 15:15:07 +0800

    2017100602

---
+++ Controllers/BankController.cs
@@ -0,0 +1,57 @@
+﻿using MaxiSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaxiSample.Controllers
+{
+    public class BankController : Controller
+    {
+        #region Bank
+        public ActionResult Bank() { return View(); }
+
+        [HttpPost]
+        public ActionResult Bank_S()
+        {
+            using (var db = CsFunc.DBExtend())
+            { return Json(db.getBank().ToArray()).JsonExtMax(); }
+        }
+
+        [HttpPost]
+        public ActionResult Bank_I(Bank_M obj)
+        {
+            using (var db = CsFunc.DBExtend())
+            {
+                var li = db.Bank_M.FirstOrDefault(s => s.cseq == obj.cseq);
+                if (obj.cseq == 0 && li == null)
+                {
+                    obj.cseq = db.Bank_M.DefaultIfEmpty().Max(s => s == null ? 0 : s.cseq) + 1;
+                    db.Bank_M.Add(obj);
+                }
+                else
+                {
+                    li.cname = obj.cname;
+                    li.camount = obj.camount;
+                    li.cenddt = obj.cenddt;
+                    li.ctype = obj.ctype;
+                    li.cdescq = obj.cdescq;
+                }
+                return Json(db.SaveChanges().MsgDone());
+            }
+        }
+        [HttpPost]
+        public ActionResult Bank_D(int? cseq)
+        {
+            using (var db = CsFunc.DBExtend())
+            {
+                var li = db.Bank_M.FirstOrDefault(s => s.cseq == cseq);
+                db.Bank_M.Remove(li);
+                return Json(db.SaveChanges().MsgDone());
+            }
+        }
+
+        #endregion Bank
+    }
+}
